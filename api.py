547cdc2d2876503d1be29e25a7bf1b6fe231b6fd
MrKolbaskin <kullerosn@gmail.com>
2020-10-03 17:35:45 +0300

    Added support NN

--- api.py
+++ api.py
@@ -3,7 +3,10 @@ from flask_restful import Api
 
 from resources.model_resource import ModelResource
 
+UPLOAD_FOLDER = 'uploads'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(app)
 
 api.add_resource(ModelResource, '/')
--- resources/model_resource.py
+++ resources/model_resource.py
@@ -1,6 +1,25 @@
+from flask import request
 from flask_restful import Resource
+import pickle
+import cv2
+import os
 
 class ModelResource(Resource):
-    @staticmethod
-    def get():
-        return {"message": "GangBank welcomes you!"}, 200
+    def __init__(self):
+        with open('MnistNN.pickle', 'rb') as f:
+            self.model = pickle.load(f)
+
+    def get(self):
+        file = request.files['file']
+        file_path = f'uploads/{file.filename}'
+        file.save(file_path)
+
+        img = cv2.imread(file_path)
+        input_nn = cv2.resize(img[:, :, 0], dsize=(28, 28), interpolation=cv2.INTER_CUBIC).reshape(1, -1) / 255
+        res = self.model.predict(input_nn)[0]
+        os.remove(file_path)
+        return {
+            "message": "GangBank welcomes you!",
+            "filemame": file.filename,
+            'result_nn': res
+            }, 200
