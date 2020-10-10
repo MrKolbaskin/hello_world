b47e1af40bb06812d594c8f071a7441d85d92224
MrKolbaskin <kullerosn@gmail.com>
2020-10-10 19:21:35 +0300

    ssl

--- api.py
+++ api.py
@@ -4,6 +4,11 @@ from flask_restful import Api
 from resources.model_resource import ModelResource
 from resources.ssl_verification import SSLResource
 
+from OpenSSL import SSL
+context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+context.use_privatekey_file('ssl/private.key')
+context.use_certificate_file('ssl/certificate.crt')
+
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
@@ -14,4 +19,4 @@ api.add_resource(ModelResource, '/')
 api.add_resource(SSLResource, '/.well-known/pki-validation/5BF4B06DA0E86C7D0ABFB5B2AF195FAC.txt')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", ssl_context=context)
