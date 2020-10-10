f028ac5a20b890187f168f5fbb9a812eed7a52ac
MrKolbaskin <kullerosn@gmail.com>
2020-10-10 20:16:11 +0300

    ssl

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sslify import SSLify
+import ssl
 
 from resources.model_resource import ModelResource
 from resources.ssl_verification import SSLResource
@@ -11,11 +12,11 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(app)
 
-context = ('ssl/certificate.crt', 'ssl/private.key')
-sslify = SSLify(app)
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.load_cert_chain('ssl/certificate.crt', 'ssl/private.key')
 
 api.add_resource(ModelResource, '/')
 api.add_resource(SSLResource, '/.well-known/pki-validation/5BF4B06DA0E86C7D0ABFB5B2AF195FAC.txt')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", ssl_context=context, port=443)
+    app.run(debug=True, host="0.0.0.0", ssl_context=context, port=5000)
