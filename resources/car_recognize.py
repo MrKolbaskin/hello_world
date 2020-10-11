9bfdefab8592103f6dd41469459bd57fc9e3f831
MrKolbaskin <kullerosn@gmail.com>
2020-10-11 12:29:01 +0300

    update

--- resources/car_recognize.py
+++ resources/car_recognize.py
@@ -14,7 +14,7 @@ class CarRecognize(Resource):
         self.label_names = ['Hyundai Solaris', 'KIA Rio', 'SKODA OCTAVIA', 'Volkswagen Polo', 'Volkswagen Tiguan']
 
     def post(self):
-        file = request.get_json['content']
+        file = request.get_json()['content']
         file_path = 'uploads/tmp.jpg'
         
         with open(file_path, 'wb') as f:
